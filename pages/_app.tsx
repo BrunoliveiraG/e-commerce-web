fe14243888bb451c107d6724ab48d5e558bbf74a
brunogomesbx <brunogomes@bxblue.com.br>
2022-01-14 14:40:32 -0300

    initial setup

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+// @ts-ignore
+import React from 'react';
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
+import Head from 'next/head';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return(
+    <>
+        <head>
+            <title>BBitGames</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
