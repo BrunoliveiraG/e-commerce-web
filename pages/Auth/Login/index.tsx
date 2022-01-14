5dcc477e21c2a29287d91ccb99a0ccc09550b3e0
brunogomesbx <brunogomes@bxblue.com.br>
2022-01-14 20:58:50 -0300

    logic authentication config

--- pages/Auth/Login/index.tsx
+++ pages/Auth/Login/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import MainComponent from "../../../components/shared/MainComponent";
 import LoginForm from "../../../components/LoginForm";
+import SignUpForm from "../../../components/SignUpForm";
 
 const Login: React.FC = () => {
   return (
     <MainComponent>
+      <br />
       <LoginForm titlePhrase="Access my account" buttonPhrase="Log In" />
+      <br />
+      <SignUpForm titlePhrase="Create an account" buttonPhrase="Sign Up" />
     </MainComponent>
   )
 }
