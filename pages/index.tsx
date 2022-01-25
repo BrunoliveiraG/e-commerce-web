bd31d56d3c911aa47ec91e6196df9a262c3bd7c9
brunogomesbx <brunogomes@bxblue.com.br>
2022-01-24 23:05:57 -0300

    product info component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import MainComponent from "../components/shared/MainComponent";
 import { useRouter } from "next/router";
 import styles from '../styles/Home.module.css'
+import ProductInfo from "../components/shared/ProductInfo";
 
 const Home: React.FC = () => {
   const router = useRouter();
 
   return (
     <MainComponent>
-      <button onClick={() => router.push('Auth/Login')}>Login</button>
+      <h1>Home</h1>
+      <ProductInfo />
+      <ProductInfo type="highlighted"/>
     </MainComponent>
   )
 }
